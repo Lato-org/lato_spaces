d0b65cc3cfd324953651773ffbc7604305fde4bc
Gregorio Galante <me@gregoriogalante.com>
2023-11-02 14:58:11 +0100

    Updates

---
+++ config/initializers/lato_monkey_patching.rb
@@ -0,0 +1,15 @@
+# TODO: make this code working!
+
+# require 'lato'
+
+# # Add method spaces_group_id and spaces_group to Lato::Session class instance.
+# Lato::Session.class_eval do
+#   def spaces_group_id
+#     @session&.dig(:spaces_group_id)
+#   end
+
+#   def spaces_group
+#     return nil if spaces_group_id.blank?
+#     LatoSpaces::Group.find_by_id(spaces_group_id)
+#   end
+# end
