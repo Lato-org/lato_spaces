e0f48ca0cfc853d6514484dda0a46905efcc365c
Gregorio Galante <me@gregoriogalante.com>
2023-11-30 08:25:21 +0100

    Small updates

--- app/models/concerns/lato_spaces/associable.rb
+++ app/models/concerns/lato_spaces/associable.rb
@@ -11,6 +11,42 @@ module LatoSpaces::Associable
     # Scopes
     ##
 
-    scope :for_lato_spaces_group, -> (group) { joins(:lato_spaces_groups).where(lato_spaces_groups: { id: group.id }) }
+    scope :for_lato_spaces_group, -> (group_id) { joins(:lato_spaces_groups).where(lato_spaces_groups: { id: group_id }) }
+    scope :with_lato_spaces_groups, -> { joins(:lato_spaces_groups) }
+    scope :without_lato_spaces_groups, -> { left_outer_joins(:lato_spaces_groups).where(lato_spaces_groups: { id: nil }) }
   end
+
+  def add_to_group(group_id)
+    association = lato_spaces_associations.create(lato_spaces_group_id: group_id)
+    unless association.valid?
+      errors.add(:base, association.errors.full_messages.join(', '))
+      return false
+    end
+
+    true
+  end
+
+  def remove_from_group(group_id)
+    association = lato_spaces_associations.find_by(lato_spaces_group_id: group_id)
+    return true unless association
+
+    unless association.destroy
+      errors.add(:base, association.errors.full_messages.join(', '))
+      return false
+    end
+
+    true
+  end
+
+  def switch_group(group_id)
+    association = lato_spaces_associations.first
+    return add_to_group(group_id) unless association
+
+    unless association.update(lato_spaces_group_id: group_id)
+      errors.add(:base, association.errors.full_messages.join(', '))
+      return false
+    end
+
+    true
+  end 
 end
